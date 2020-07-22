4c6e1566620e716394194379e7ece27d9c6d06ca
Kiran V Garimella <kiran@broadinstitute.org>
2020-07-22 14:04:28 -0400

    All against all alignment of samples to references

--- data/sim.sh
+++ data/sim.sh
@@ -26,12 +26,12 @@ do
     for sn in A B
     do
         ~/repositories/readsim/readsim -r $haps/ref_hap.fasta -l $rl -d $d -e 0.001 ${reads}/sample_${sn}.ref_imperfect
-        ~/repositories/minimap2/minimap2 -ayYL --MD --eqx -x sr -R '@RG\tSM:${sn}\tID:ref_imperfect' $haps/ref_hap.fasta ${reads}/sample_${sn}.ref_imperfect.1.fa.gz ${reads}/sample_${sn}.ref_imperfect.2.fa.gz | samtools sort | samtools view -b > ${reads}/sample_${sn}.ref_imperfect.bam && samtools index ${reads}/sample_${sn}.ref_imperfect.bam
+        #~/repositories/minimap2/minimap2 -ayYL --MD --eqx -x sr -R '@RG\tSM:${sn}\tID:ref_imperfect' $haps/ref_hap.fasta ${reads}/sample_${sn}.ref_imperfect.1.fa.gz ${reads}/sample_${sn}.ref_imperfect.2.fa.gz | samtools sort | samtools view -b > ${reads}/sample_${sn}.ref_imperfect.bam && samtools index ${reads}/sample_${sn}.ref_imperfect.bam
 
-        mccortex31 build -f -m 1G -k 31 -S -s ${sn} -2 ${reads}/sample_${sn}.ref_imperfect.1.fa.gz:${reads}/sample_${sn}.ref_imperfect.2.fa.gz ${asm}/sample_${sn}.ref_imperfect.ctx
-        mccortex31 clean -f -m 1G -o ${asm}/sample_${sn}.ref_imperfect.cleaned.ctx ${asm}/sample_${sn}.ref_imperfect.ctx
-        mccortex31 inferedges -f -m 1G -o ${asm}/sample_${sn}.ref_imperfect.inferedges.ctx ${asm}/sample_${sn}.ref_imperfect.cleaned.ctx
-        mccortex31 thread -f -m 4G -W -2 ${reads}/sample_${sn}.ref_imperfect.1.fa.gz:${reads}/sample_${sn}.ref_imperfect.2.fa.gz -o ${asm}/sample_${sn}.ref_imperfect.inferedges.ctp.gz ${asm}/sample_${sn}.ref_imperfect.inferedges.ctx 
+        #mccortex31 build -f -m 1G -k 31 -S -s ${sn} -2 ${reads}/sample_${sn}.ref_imperfect.1.fa.gz:${reads}/sample_${sn}.ref_imperfect.2.fa.gz ${asm}/sample_${sn}.ref_imperfect.ctx
+        #mccortex31 clean -f -m 1G -o ${asm}/sample_${sn}.ref_imperfect.cleaned.ctx ${asm}/sample_${sn}.ref_imperfect.ctx
+        #mccortex31 inferedges -f -m 1G -o ${asm}/sample_${sn}.ref_imperfect.inferedges.ctx ${asm}/sample_${sn}.ref_imperfect.cleaned.ctx
+        #mccortex31 thread -f -m 4G -W -2 ${reads}/sample_${sn}.ref_imperfect.1.fa.gz:${reads}/sample_${sn}.ref_imperfect.2.fa.gz -o ${asm}/sample_${sn}.ref_imperfect.inferedges.ctp.gz ${asm}/sample_${sn}.ref_imperfect.inferedges.ctx 
     done
 
     if [[ -f "$alt1_file" ]]; then
@@ -39,13 +39,13 @@ do
 
         for sn in C D
         do
-            ~/repositories/readsim/readsim -r $haps/alt1_hap.fasta -l $rl -d $d -e 0.001 ${reads}/sample_${sn}.alt1_imperfect
-            ~/repositories/minimap2/minimap2 -ayYL --MD --eqx -x sr -R '@RG\tSM:${sn}\tID:alt1_imperfect' $haps/alt1_hap.fasta ${reads}/sample_${sn}.alt1_imperfect.1.fa.gz ${reads}/sample_${sn}.alt1_imperfect.2.fa.gz | samtools sort | samtools view -b > ${reads}/sample_${sn}.alt1_imperfect.bam && samtools index ${reads}/sample_${sn}.alt1_imperfect.bam
+            ~/repositories/readsim/readsim -r $haps/alt1_hap.fasta -l $rl -d $d -e 0.001 ${reads}/sample_${sn}.alt_imperfect
+            #~/repositories/minimap2/minimap2 -ayYL --MD --eqx -x sr -R '@RG\tSM:${sn}\tID:alt1_imperfect' $haps/alt1_hap.fasta ${reads}/sample_${sn}.alt1_imperfect.1.fa.gz ${reads}/sample_${sn}.alt1_imperfect.2.fa.gz | samtools sort | samtools view -b > ${reads}/sample_${sn}.alt1_imperfect.bam && samtools index ${reads}/sample_${sn}.alt1_imperfect.bam
 
-            mccortex31 build -f -m 1G -k 31 -S -s ${sn} -2 ${reads}/sample_${sn}.alt1_imperfect.1.fa.gz:${reads}/sample_${sn}.alt1_imperfect.2.fa.gz ${asm}/sample_${sn}.alt1_imperfect.ctx
-            mccortex31 clean -f -m 1G -o ${asm}/sample_${sn}.alt1_imperfect.cleaned.ctx ${asm}/sample_${sn}.alt1_imperfect.ctx
-            mccortex31 inferedges -f -m 1G -o ${asm}/sample_${sn}.alt1_imperfect.inferedges.ctx ${asm}/sample_${sn}.alt1_imperfect.cleaned.ctx
-            mccortex31 thread -f -m 4G -W -2 ${reads}/sample_${sn}.alt1_imperfect.1.fa.gz:${reads}/sample_${sn}.alt1_imperfect.2.fa.gz -o ${asm}/sample_${sn}.alt1_imperfect.inferedges.ctp.gz ${asm}/sample_${sn}.alt1_imperfect.inferedges.ctx 
+            #mccortex31 build -f -m 1G -k 31 -S -s ${sn} -2 ${reads}/sample_${sn}.alt1_imperfect.1.fa.gz:${reads}/sample_${sn}.alt1_imperfect.2.fa.gz ${asm}/sample_${sn}.alt1_imperfect.ctx
+            #mccortex31 clean -f -m 1G -o ${asm}/sample_${sn}.alt1_imperfect.cleaned.ctx ${asm}/sample_${sn}.alt1_imperfect.ctx
+            #mccortex31 inferedges -f -m 1G -o ${asm}/sample_${sn}.alt1_imperfect.inferedges.ctx ${asm}/sample_${sn}.alt1_imperfect.cleaned.ctx
+            #mccortex31 thread -f -m 4G -W -2 ${reads}/sample_${sn}.alt1_imperfect.1.fa.gz:${reads}/sample_${sn}.alt1_imperfect.2.fa.gz -o ${asm}/sample_${sn}.alt1_imperfect.inferedges.ctp.gz ${asm}/sample_${sn}.alt1_imperfect.inferedges.ctx 
         done
     fi
 
@@ -54,13 +54,13 @@ do
 
         for sn in E F
         do
-            ~/repositories/readsim/readsim -r $haps/alt2_hap.fasta -l $rl -d $d -e 0.001 ${reads}/sample_${sn}.alt2_imperfect
-            ~/repositories/minimap2/minimap2 -ayYL --MD --eqx -x sr -R '@RG\tSM:${sn}\tID:alt2_imperfect' $haps/alt2_hap.fasta ${reads}/sample_${sn}.alt2_imperfect.1.fa.gz ${reads}/sample_${sn}.alt2_imperfect.2.fa.gz | samtools sort | samtools view -b > ${reads}/sample_${sn}.alt2_imperfect.bam && samtools index ${reads}/sample_${sn}.alt2_imperfect.bam
+            ~/repositories/readsim/readsim -r $haps/alt2_hap.fasta -l $rl -d $d -e 0.001 ${reads}/sample_${sn}.alt_imperfect
+            #~/repositories/minimap2/minimap2 -ayYL --MD --eqx -x sr -R '@RG\tSM:${sn}\tID:alt2_imperfect' $haps/alt2_hap.fasta ${reads}/sample_${sn}.alt2_imperfect.1.fa.gz ${reads}/sample_${sn}.alt2_imperfect.2.fa.gz | samtools sort | samtools view -b > ${reads}/sample_${sn}.alt2_imperfect.bam && samtools index ${reads}/sample_${sn}.alt2_imperfect.bam
 
-            mccortex31 build -f -m 1G -k 31 -S -s ${sn} -2 ${reads}/sample_${sn}.alt2_imperfect.1.fa.gz:${reads}/sample_${sn}.alt2_imperfect.2.fa.gz ${asm}/sample_${sn}.alt2_imperfect.ctx
-            mccortex31 clean -f -m 1G -o ${asm}/sample_${sn}.alt2_imperfect.cleaned.ctx ${asm}/sample_${sn}.alt2_imperfect.ctx
-            mccortex31 inferedges -f -m 1G -o ${asm}/sample_${sn}.alt2_imperfect.inferedges.ctx ${asm}/sample_${sn}.alt2_imperfect.cleaned.ctx
-            mccortex31 thread -f -m 4G -W -2 ${reads}/sample_${sn}.alt2_imperfect.1.fa.gz:${reads}/sample_${sn}.alt2_imperfect.2.fa.gz -o ${asm}/sample_${sn}.alt2_imperfect.inferedges.ctp.gz ${asm}/sample_${sn}.alt2_imperfect.inferedges.ctx 
+            #mccortex31 build -f -m 1G -k 31 -S -s ${sn} -2 ${reads}/sample_${sn}.alt2_imperfect.1.fa.gz:${reads}/sample_${sn}.alt2_imperfect.2.fa.gz ${asm}/sample_${sn}.alt2_imperfect.ctx
+            #mccortex31 clean -f -m 1G -o ${asm}/sample_${sn}.alt2_imperfect.cleaned.ctx ${asm}/sample_${sn}.alt2_imperfect.ctx
+            #mccortex31 inferedges -f -m 1G -o ${asm}/sample_${sn}.alt2_imperfect.inferedges.ctx ${asm}/sample_${sn}.alt2_imperfect.cleaned.ctx
+            #mccortex31 thread -f -m 4G -W -2 ${reads}/sample_${sn}.alt2_imperfect.1.fa.gz:${reads}/sample_${sn}.alt2_imperfect.2.fa.gz -o ${asm}/sample_${sn}.alt2_imperfect.inferedges.ctp.gz ${asm}/sample_${sn}.alt2_imperfect.inferedges.ctx 
         done
     fi
 
@@ -69,16 +69,33 @@ do
 
         for sn in G H
         do
-            ~/repositories/readsim/readsim -r $haps/alt3_hap.fasta -l $rl -d $d -e 0.001 ${reads}/sample_${sn}.alt3_imperfect
-            ~/repositories/minimap2/minimap2 -ayYL --MD --eqx -x sr -R '@RG\tSM:${sn}\tID:alt3_imperfect' $haps/alt3_hap.fasta ${reads}/sample_${sn}.alt3_imperfect.1.fa.gz ${reads}/sample_${sn}.alt3_imperfect.2.fa.gz | samtools sort | samtools view -b > ${reads}/sample_${sn}.alt3_imperfect.bam && samtools index ${reads}/sample_${sn}.alt3_imperfect.bam
+            ~/repositories/readsim/readsim -r $haps/alt3_hap.fasta -l $rl -d $d -e 0.001 ${reads}/sample_${sn}.alt_imperfect
+            #~/repositories/minimap2/minimap2 -ayYL --MD --eqx -x sr -R '@RG\tSM:${sn}\tID:alt3_imperfect' $haps/alt3_hap.fasta ${reads}/sample_${sn}.alt3_imperfect.1.fa.gz ${reads}/sample_${sn}.alt3_imperfect.2.fa.gz | samtools sort | samtools view -b > ${reads}/sample_${sn}.alt3_imperfect.bam && samtools index ${reads}/sample_${sn}.alt3_imperfect.bam
 
-            mccortex31 build -f -m 1G -k 31 -S -s ${sn} -2 ${reads}/sample_${sn}.alt3_imperfect.1.fa.gz:${reads}/sample_${sn}.alt3_imperfect.2.fa.gz ${asm}/sample_${sn}.alt3_imperfect.ctx
-            mccortex31 clean -f -m 1G -o ${asm}/sample_${sn}.alt3_imperfect.cleaned.ctx ${asm}/sample_${sn}.alt3_imperfect.ctx
-            mccortex31 inferedges -f -m 1G -o ${asm}/sample_${sn}.alt3_imperfect.inferedges.ctx ${asm}/sample_${sn}.alt3_imperfect.cleaned.ctx
-            mccortex31 thread -f -m 4G -W -2 ${reads}/sample_${sn}.alt3_imperfect.1.fa.gz:${reads}/sample_${sn}.alt3_imperfect.2.fa.gz -o ${asm}/sample_${sn}.alt3_imperfect.inferedges.ctp.gz ${asm}/sample_${sn}.alt3_imperfect.inferedges.ctx 
+            #mccortex31 build -f -m 1G -k 31 -S -s ${sn} -2 ${reads}/sample_${sn}.alt3_imperfect.1.fa.gz:${reads}/sample_${sn}.alt3_imperfect.2.fa.gz ${asm}/sample_${sn}.alt3_imperfect.ctx
+            #mccortex31 clean -f -m 1G -o ${asm}/sample_${sn}.alt3_imperfect.cleaned.ctx ${asm}/sample_${sn}.alt3_imperfect.ctx
+            #mccortex31 inferedges -f -m 1G -o ${asm}/sample_${sn}.alt3_imperfect.inferedges.ctx ${asm}/sample_${sn}.alt3_imperfect.cleaned.ctx
+            #mccortex31 thread -f -m 4G -W -2 ${reads}/sample_${sn}.alt3_imperfect.1.fa.gz:${reads}/sample_${sn}.alt3_imperfect.2.fa.gz -o ${asm}/sample_${sn}.alt3_imperfect.inferedges.ctp.gz ${asm}/sample_${sn}.alt3_imperfect.inferedges.ctx 
         done
     fi
 
-    mccortex31 join -f -m 4G -S -o ${asm}/final_joined.imperfect.ctx ${asm}/sample_*imperfect.inferedges.ctx
-    mccortex31 view -k ${asm}/final_joined.imperfect.ctx > ${asm}/final_joined.imperfect.txt
+    for allele_type in ref alt1 alt2 alt3
+    do
+        fasta="${haps}/${allele_type}_hap.fasta"
+
+        if [[ -f "$fasta" ]]; then
+            for sn in A B C D E F G H
+            do
+                reads1="${reads}/sample_${sn}.alt_imperfect.1.fa.gz"
+                reads2="${reads}/sample_${sn}.alt_imperfect.2.fa.gz"
+
+                if [[ -f "$reads1" ]]; then
+                    ~/repositories/minimap2/minimap2 -ayYL --MD --eqx -x sr -R '@RG\tSM:${sn}\tID:${allele_type}_imperfect' $fasta ${reads1} ${reads2} | samtools sort | samtools view -b > ${reads}/sample_${sn}.${allele_type}_imperfect.bam && samtools index ${reads}/sample_${sn}.${allele_type}_imperfect.bam
+                fi
+            done
+        fi
+    done
+
+    #mccortex31 join -f -m 4G -S -o ${asm}/final_joined.imperfect.ctx ${asm}/sample_*imperfect.inferedges.ctx
+    #mccortex31 view -k ${asm}/final_joined.imperfect.ctx > ${asm}/final_joined.imperfect.txt
 done
